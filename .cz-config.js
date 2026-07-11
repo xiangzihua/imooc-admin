12393b8056b5dd041c9ac337f46222a7a0ef488d
xiangzihua <2458106813@qq.com>
2026-07-11 16:06:36 +0800

    style: 修改文字显示错误

--- .cz-config.js
+++ .cz-config.js
@@ -5,7 +5,10 @@ module.exports = {
     { value: 'fix', name: 'fix:      修复' },
     { value: 'docs', name: 'docs:     文档变更' },
     { value: 'style', name: 'style:    代码格式(不影响代码运行的变动)' },
-    { value: 'refactor', name: 'refactor: 重构(既不是增加feature，也不是修复bug)' },
+    {
+      value: 'refactor',
+      name: 'refactor: 重构(既不是增加feature，也不是修复bug)'
+    },
     { value: 'perf', name: 'perf:     性能优化' },
     { value: 'test', name: 'test:     增加测试' },
     { value: 'chore', name: 'chore:    构建过程或辅助工具的变动' },
@@ -19,7 +22,7 @@ module.exports = {
     subject: '请简要描述提交(必填)',
     body: '请输入详细描述(可选)',
     footer: '请输入要关闭的issue(可选)',
-    confirmCommit: '取人使用以上信息提交？(y/n/e/h)'
+    confirmCommit: '确认使用以上信息提交？(y/n/e/h)'
   },
   // 跳过问题
   skipQuestions: ['body', 'footer'],
